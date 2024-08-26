6e194ca32dda3e0705be7cbd74a7577612620199
Pana <ghostninja698@gmial.com>
2024-08-26 15:22:05 +0100

    path fix

--- app.py
+++ app.py
@@ -368,7 +368,7 @@ chrome_options.add_argument("--window-size=1920x1080")
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
 
-service = Service("")
+service = Service("drivers//chromedriver.exe")
 driver = webdriver.Chrome(service=service, options=chrome_options)
 
 if 'location' not in st.session_state:
